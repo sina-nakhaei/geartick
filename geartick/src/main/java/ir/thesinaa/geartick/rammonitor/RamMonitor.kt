bd96d43d97c750c83663d591168d9ecc1eb8d97d
Sina Nakhaei <cna.nakhaei@gmail.com>
2025-03-01 22:15:27 +0330

    implement ram monitor

---
+++ geartick/src/main/java/ir/thesinaa/geartick/rammonitor/RamMonitor.kt
@@ -0,0 +1,44 @@
+package ir.thesinaa.geartick.rammonitor
+
+import android.app.ActivityManager
+import android.content.Context
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+
+class RamMonitor(context: Context) {
+    private var monitoringJob: Job? = null
+    private val activityManager =
+        context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+
+    private val memoryInfo = ActivityManager.MemoryInfo()
+
+    init { activityManager.getMemoryInfo(memoryInfo) }
+
+    fun launch(
+        context: CoroutineContext = Dispatchers.IO,
+        delayMillis: Long = 1000,
+        onRamUpdate: (used: Long, available: Long) -> Unit
+    ) {
+        monitoringJob = CoroutineScope(context).launch {
+            while (isActive) {
+                withContext(Dispatchers.Main) {
+                    val totalRam = memoryInfo.totalMem.toMegaByte()
+                    val available = memoryInfo.availMem.toMegaByte()
+                    val used = totalRam - available
+
+                    onRamUpdate(used, available)
+                }
+
+                delay(delayMillis)
+            }
+        }
+    }
+
+    fun stop() {
+        monitoringJob?.cancel()
+    }
+}
+
+private fun Long.toMegaByte(): Long {
+    return this / (1024 * 1024)
+}
